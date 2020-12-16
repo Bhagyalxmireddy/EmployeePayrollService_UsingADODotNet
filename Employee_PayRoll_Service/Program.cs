b5cdb58e81566eb6f23758f8dc236cba19638199
Bhagi <puppyreddy1997@gmail.com>
2020-12-16 16:20:16 +0530

    [Bhagyalaxmi]ADDED:Printing the welcome message

---
+++ Employee_PayRoll_Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_PayRoll_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to EmployeePayroll");
+        }
+    }
+}
