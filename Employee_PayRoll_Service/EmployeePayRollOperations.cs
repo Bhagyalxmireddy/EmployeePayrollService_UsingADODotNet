15ee7c8d68be784fcecc614bd87d1ad23fcd3804
Bhagi <puppyreddy1997@gmail.com>
2020-12-20 20:03:59 +0530

    [Bhagyalaxmi]ADDED:Adding employess To DB Without thearding

---
+++ Employee_PayRoll_Service/EmployeePayRollOperations.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Employee_PayRoll_Service
+{
+    public class EmployeePayRollOperations
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Payroll_Services;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public bool addEmployee(EmployeeModel model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("spAddEmployeeDetails", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Name", model.Name);
+                    command.Parameters.AddWithValue("@Gender", model.Gender);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@StartDate", model.StartDate);
+                    command.Parameters.AddWithValue("@Department", model.Department);
+                    command.Parameters.AddWithValue("@Basic_Pay", model.Basic_Pay);
+                    command.Parameters.AddWithValue("@Deductions", model.Deductions);
+                    command.Parameters.AddWithValue("@IncomeTax", model.IncomeTax);
+                    command.Parameters.AddWithValue("@TaxablePay", model.TaxablePay);
+                    command.Parameters.AddWithValue("@NetPay", model.NetPay);
+
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    if (!result.Equals(0))
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+        public bool AddMultipleElementToDB(List<EmployeeModel> Models)
+        {
+            foreach (EmployeeModel employee in Models)
+            {
+                Console.WriteLine("Employee being added:" + employee.Name);
+                bool result = addEmployee(employee);
+                Console.WriteLine("Employee Added:" + employee.Name);
+                if (result == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public List<EmployeeModel> listemployeeModel = new List<EmployeeModel>();
+        public void AddEmployeePayroll(List<EmployeeModel> listemployeeModel)
+        {
+            listemployeeModel.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee begin: " + employeeData.Name);
+                this.AddEmployee(employeeData);
+                Console.WriteLine("employee Added : " + employeeData.Name);
+            });
+            Console.WriteLine(this.listemployeeModel.ToString());
+        }
+
+        public void AddEmployee(EmployeeModel employeeData)
+        {
+            listemployeeModel.Add(employeeData);
+        }
+
+        public void AddEmployeePayrollwithTheard(List<EmployeeModel> listemployeeModel)
+        {
+            listemployeeModel.ForEach(employeeData =>
+            {
+                Task thread = new Task(() =>
+                {
+
+                    Console.WriteLine("Employee begin: " + employeeData.Name);
+                    this.AddEmployee(employeeData);
+                    Console.WriteLine("employee Added : " + employeeData.Name);
+                });
+                thread.Start();
+            });
+            Console.WriteLine(this.listemployeeModel.Count);
+        }
+
+    }
+}
